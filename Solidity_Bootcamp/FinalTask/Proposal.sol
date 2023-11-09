b82020284ca747051cce9573abf237e9917ed4a4
gungorcaglar <gungorcaglar@hotmail.com.tr>
2023-11-09 15:20:58 +0300

    Final Task

--- Solidity_Bootcamp/FinalTask/Proposal.sol
+++ Solidity_Bootcamp/FinalTask/Proposal.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.18;
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-
 contract ProposalContract {
     address owner;
 
